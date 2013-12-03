3f68c442c815ec296116fe6208281c29f02d6c9c
IceDragon200 <mistdragon100@gmail.com>
2013-12-03 10:19:37 -0500

    Updated KeysController with CRUD actions
    Everything except an update action

--- app/controllers/api/keys_controller.rb
+++ app/controllers/api/keys_controller.rb
@@ -1,25 +1,38 @@
 class Api::KeysController < ApiController
 
+  before_action :find_key, only: [:show, :destroy]
+
   def test
     render text: current_user.sign_in_count
   end
 
-  def index
-    render status: 200, json: current_user.keys.to_json
-  end
-
   def create
     if !Key.where(fingerprint: params[:fingerprint]).exists?
-
-      Key.create!(
-        user: current_user,
-        key: params[:key],
-        fingerprint: params[:fingerprint]
-      )
-
+      Key.create!(user: current_user,
+                  key: params[:key],
+                  fingerprint: params[:fingerprint])
       render status: 200, text: "Added pubkey!"
     else
       render status: 409, text: "Key already exists!"
     end
   end
+
+  def index
+    @keys = current_user.keys
+    render status: 200
+  end
+
+  def show
+    render 'key'
+  end
+
+  def destroy
+    @key.destroy
+  end
+
+  def find_key
+    @key = Key.find(params[:key][:id])
+  end
+  private :find_key
+
 end
