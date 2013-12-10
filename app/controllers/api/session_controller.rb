04a894266158f85854b2a9ebd88d9197b517858f
IceDragon200 <mistdragon100@gmail.com>
2013-12-10 17:03:34 -0500

    Updated login, we now use the username instead of email

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -3,10 +3,11 @@ class Api::SessionController < ApiController
   skip_before_action :authenticate_user_from_api_key!
 
   def create
-    user = User.find_by(email: params[:email])
-
-    if user.valid_password?(params[:password])
-      render :text => {api_key: user.api_key}.to_json
+    user = User.find_by(username: params[:username])
+    if user && user.valid_password?(params[:password])
+      render json: { api_key: user.api_key }, status: 200
+    else
+      head 400
     end
   end
 
