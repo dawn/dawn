7898cd6a6974f0b17ffa43c735385018f0d127a9
IceDragon200 <mistdragon100@gmail.com>
2013-12-11 15:50:09 -0500

    Updated App#scale method

--- app/models/app.rb
+++ app/models/app.rb
@@ -83,34 +83,22 @@ class App
   end
 
   # scales the application to a particular size (in gears)
-  def scale input
+  def scale(options)
     old_formation = self.formation
-
-    pairs = input.split(/\s+/)
-
-    new_formation = pairs.each_with_object({}) do |str, hash|
-      key, val = str.split("=")
-      hash[key.to_sym] = val.to_i # TODO: parse +1, -2 as relative
-    end  # rescue {}
-
-    new_formation.each_pair do |type, count|
-      if old_formation[key]
-        diff = old_formation[key] - count
+    options.each do |gear_type, count|
+      if o = old_formation[gear_type]
+        diff = count - o
       else
         diff = count
       end
-
       # determine whether we need to add or remove gears
-      if diff == 0
-        # do nothing
-      elsif diff > 0
-        diff.times { gear.create!(type: type) }
+      if diff > 0
+        diff.times { gear.create!(type: gear_type) }
       elsif diff < 0
         # get rid of diff number of gears, from the highest worker number down
-        gears.where(type: type).order_by(:number.desc).limit(abs(diff)).destroy
+        gears.where(type: gear_type).order_by(:number.desc).limit(abs(diff)).destroy
       end
     end
-
     self.formation = new_formation
   end
 
