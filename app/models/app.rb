32ae9731fc7f142669434b79f01a1dd2b249c5df
Blaž Hrastnik <speed.the.bboy@gmail.com>
2013-12-12 01:05:36 +0100

    will you work now? please

--- app/models/app.rb
+++ app/models/app.rb
@@ -50,7 +50,7 @@ class App
     # `docker tag #{self.image} `
 
     # set the release version to the counter
-    releases.create(image: image_name, version: version)
+    self.releases.create(image: image_name, version: version)
   end
 
   # using the latest release, destroy old gears and
