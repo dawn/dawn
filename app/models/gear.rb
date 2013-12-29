7009129472987fcc94698a0deb9ec0e1d886787d
IceDragon200 <mistdragon100@gmail.com>
2013-12-28 19:59:21 -0500

    Initialize the started_at with the Time.now as default

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -43,7 +43,7 @@ class Gear
   field :port,         type: Integer # outbound port of the container
   field :ip,           type: String  # network IP of the container
   field :container_id, type: String  # pid/identifier of the Docker container
-  field :started_at,   type: Time
+  field :started_at,   type: Time,   default: { Time.now }
 
   def name # full name: web.1, mailer.3 (type.number)
     "#{type}.#{number}"
