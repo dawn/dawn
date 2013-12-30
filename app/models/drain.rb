bb6eb00923962e469a366587f556db7ae02fc8d5
IceDragon200 <mistdragon100@gmail.com>
2013-12-29 20:30:07 -0500

    Drain#name generation and validations

--- app/models/drain.rb
+++ app/models/drain.rb
@@ -1,11 +1,29 @@
 class Drain
 
+  ## mixins
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  ## ownership
   belongs_to :app
 
+  ## attributes
   field :name, type: String # Should drains even have a name? eg. McSwirly
   field :url,  type: String # Where does this drain lead?
+
+  ## validation
+  validates :url, #uniqueness: true,
+                  presence: true
+  ## validation-callback
+  before_validation :ensure_name, unless: Proc.new { |model| model.persisted? }
+
+  def ensure_name
+    if name.blank?
+      loop do
+        self.name = Forgery(:dawn).drain_name
+        break name if valid?
+      end
+    end
+  end
 
 end
