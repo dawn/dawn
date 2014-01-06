643495971f464b588900423efa12cada76e09300
IceDragon200 <mistdragon100@gmail.com>
2014-01-06 13:16:29 -0500

    Key uses gitlab-keys for key management

--- app/models/key.rb
+++ app/models/key.rb
@@ -2,23 +2,21 @@ class Key # SSH public key representation
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  field :key, type: String
+  field :fingerprint, type: String
+
+  def gitlab_keys(arg)
+    system("/opt/gitlab-shell/bin/gitlab-keys #{arg}")
+  end
+  private :gitlab_keys
+
   after_create do # add the key to authorized_keys
-    Dir.chdir Dir.home("git") do
-      File.open('.ssh/authorized_keys', 'a') { |io| io.write key }
-    end
+    gitlab_keys("add-key #{key}")
   end
 
   before_destroy do # remove the key from authorized_keys (HAXX)
-    Dir.chdir Dir.home("git") do
-      File.open('.ssh/authorized_keys', 'w+') do |io|
-        contents = io.read.sub(key, '')
-        io.write contents
-      end
-    end
+    gitlab_keys("rm-key #{key}")
   end
-
-  field :key, type: String
-  field :fingerprint, type: String
 
   belongs_to :user
 end
