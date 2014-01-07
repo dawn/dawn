15978806199e4c7c74ed3252e9d828028d81c954
IceDragon200 <mistdragon100@gmail.com>
2014-01-07 18:14:33 -0500

    key not @key

--- app/controllers/api/git/git_controller.rb
+++ app/controllers/api/git/git_controller.rb
@@ -5,7 +5,7 @@ class Api::Git::GitController < ActionController::Metal
   def allowed
     key = Key.where(id: params[:key_id]).first
     return head 404 unless key
-    return head 403 unless @key.user.apps.where(name: params[:project]).exists?
+    return head 403 unless key.user.apps.where(name: params[:project]).exists?
     action  = params[:action]
     ref     = params[:ref]
     head 200
