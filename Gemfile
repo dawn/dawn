972a2818496e7072c8d7384e9cb43d005f0114b6
Blaž Hrastnik <blaz.hrast@gmail.com>
2014-06-01 21:22:17 +0100

    remove sprockets!

--- Gemfile
+++ Gemfile
@@ -11,9 +11,6 @@ gem 'devise', '~> 3.2.4'
 gem 'excon', '~> 0.33.0'
 gem 'rabl', '~> 0.10.0'
 
-# assets
-gem 'sass-rails', '~> 4.0.3'
-
 gem 'rfc822', '~> 0.1.5'  # email verification gem
 gem 'forgery', '~> 0.6.0' # random name generation
 gem 'sshkey', '~> 1.6.1'  # SSHKey Generation
--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require File.expand_path('../boot', __FILE__)
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "sprockets/railtie"
+#require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
