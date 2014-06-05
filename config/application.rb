a54cd7a56e2e625c3dc67f9084d6aeffcad73f85
Blaž Hrastnik <blaz.hrast@gmail.com>
2014-06-05 23:17:18 +0100

    remove session related middleware
    that's browser related, we always use API tokens

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,10 @@ module Dawn
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Remove session related middleware
+    config.middleware.delete "ActionDispatch::Cookies"
+    config.middleware.delete "ActionDispatch::Session::CookieStore"
+    config.middleware.delete "ActionDispatch::Flash"
   end
 end
