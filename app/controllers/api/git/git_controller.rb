200137b7f8c879baf3869839bcd5209238b4e915
IceDragon200 <mistdragon100@gmail.com>
2014-06-12 12:24:52 -0500

    Added /api/git/api_key route
    This route will fetch a User's api_key, provided a username and a build_token.
    
    Currently, DAWN_BUILD_TOKEN is a place holder.

--- app/controllers/api/git/git_controller.rb
+++ app/controllers/api/git/git_controller.rb
@@ -18,4 +18,20 @@ class Api::Git::GitController < ActionController::Metal
     user = key.user
     render text: {name: user.username}.to_json, status: 200
   end
+
+  def api_key
+    if user = User.where(username: params[:username]).first
+      if params[:build_token] == ENV["DAWN_BUILD_TOKEN"]
+        render json: { user: { api_key: user.api_key } }, status: 200
+      else
+        response = { id: "build_token.mismatch"
+                     message: "provided build_token was invalid" }
+        render json: response, status: 400
+      end
+    else
+      response = { id: "user.not_exist"
+                   message: "User (username: #{params[:username]}) does not exist" }
+      render json: response, status: 404
+    end
+  end
 end
