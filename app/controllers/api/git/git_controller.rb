060fe98d42acecbeadc7259dca0b38ac098b550e
IceDragon200 <mistdragon100@gmail.com>
2014-06-12 21:39:08 -0500

    Fixed syntax error in git_controller

--- app/controllers/api/git/git_controller.rb
+++ app/controllers/api/git/git_controller.rb
@@ -24,12 +24,12 @@ class Api::Git::GitController < ActionController::Metal
       if params[:build_token] == ENV["DAWN_BUILD_TOKEN"]
         render json: { user: { api_key: user.api_key } }, status: 200
       else
-        response = { id: "build_token.mismatch"
+        response = { id: "build_token.mismatch",
                      message: "provided build_token was invalid" }
         render json: response, status: 400
       end
     else
-      response = { id: "user.not_exist"
+      response = { id: "user.not_exist",
                    message: "User (username: #{params[:username]}) does not exist" }
       render json: response, status: 404
     end
