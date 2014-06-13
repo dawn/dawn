4dad6db19bbd7100ce7b0e9e5099b469ba5cf6fa
IceDragon200 <mistdragon100@gmail.com>
2014-06-12 21:39:19 -0500

    Simplified allowed action

--- app/controllers/api/git/git_controller.rb
+++ app/controllers/api/git/git_controller.rb
@@ -4,12 +4,11 @@ class Api::Git::GitController < ActionController::Metal
   include ActionController::Rendering   # enables rendering
 
   def allowed
-    key = Key.where(id: params[:key_id]).first
-    return head 404 unless key
-    return head 403 unless key.user.apps.where(name: params[:project]).exists?
-    action  = params[:action]
-    ref     = params[:ref]
-    render text: 'true', status: 200
+    if key = Key.where(key: params[:key]).first
+      head 200
+    else
+      head 404
+    end
   end
 
   def discover
