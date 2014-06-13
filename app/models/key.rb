558f6672eb04671661d207d5380d9aaf6211a636
IceDragon200 <mistdragon100@gmail.com>
2014-06-12 23:26:09 -0500

    Cleared out Key

--- app/models/key.rb
+++ app/models/key.rb
@@ -1,16 +1,5 @@
-class Key < ActiveRecord::Base # SSH public key representation
-
-  private def gitlab_keys(arg)
-    system("/opt/gitlab-shell/bin/gitlab-keys #{arg}")
-  end
-
-  after_create do
-    gitlab_keys("add-key \"key-#{id}\" \"#{key}\"")
-  end
-
-  before_destroy do
-    gitlab_keys("rm-key \"key-#{id}\"")
-  end
+# SSH public key representation
+class Key < ActiveRecord::Base
 
   belongs_to :user
 
