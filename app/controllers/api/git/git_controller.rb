40be79379c00e399ee0b55775fcdbf4e97b8a1d3
IceDragon200 <mistdragon100@gmail.com>
2014-06-16 15:46:26 -0500

    included ActionController::Helpers in GitController

--- app/controllers/api/git/git_controller.rb
+++ app/controllers/api/git/git_controller.rb
@@ -2,6 +2,7 @@ class Api::Git::GitController < ActionController::Metal
   include ActionController::Head        # for header only responses
   include AbstractController::Rendering
   include ActionController::Rendering   # enables rendering
+  include ActionController::Helpers
 
   helper GitHelper
 
